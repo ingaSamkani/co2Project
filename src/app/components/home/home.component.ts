dc2cd94504100a272073fa0e83fb77483d2e3a85
ingaSamkani <isamkani@gmail.com>
2019-08-17 12:23:09 +0300

    added http requests

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SubmitItem } from "../../models/models";
+import { BackendGateService } from "../../services/backend-gate.service";
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,22 @@ export class HomeComponent implements OnInit{
   public title: string = 'this is a title!!!';
   public isMenuOpened: boolean = false;
   public isFirstLoad: boolean = true;
-  constructor() {}
+
+  constructor(private backendGateService: BackendGateService) {}
 
   ngOnInit() {}
 
   public toggleMenu() {
     this.isFirstLoad = false;
     this.isMenuOpened = !this.isMenuOpened;
+  }
+
+  public onSubmit(submitItem: SubmitItem) {
+      this.backendGateService.getGasResults(submitItem).subscribe((data) => {
+        console.log("getGasResults - DATA: ", data);
+      },
+        (error) => {
+          console.log("getGasResults - ERROR: ", error);
+        })
   }
 }
--- src/app/services/backend-gate.service.ts
+++ src/app/services/backend-gate.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {SubmitItem} from "../models/models";
+import {HttpClient, HttpRequest} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendGateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
-
+  public getGasResults(submitItem: SubmitItem) {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const req = new HttpRequest('GET', url, {
+      reportProgress: true
+    });
+    return this.http.request(req);
+  }
 }
